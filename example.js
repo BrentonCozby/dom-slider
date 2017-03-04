a1b2d2c8cdb2711eceb11160a107dc07db74a218
Brenton Cozby <brenton.cozby@gmail.com>
2017-03-04 00:07:29 -0500

    Give 'em the business

---
+++ example.js
@@ -0,0 +1,30 @@
+// import slideDownUpInit from './slide-down-up'
+
+slideDownUpInit()
+
+const box = document.querySelector('.box')
+const content = document.querySelector('.content')
+const paragraphs = document.querySelectorAll('.content p')
+
+const slideDownButton = document.querySelector('.slide-down-button')
+const slideUpButton = document.querySelector('.slide-up-button')
+const slideToggleButton = document.querySelector('.slide-toggle-button')
+const toggleParagraphsButton = document.querySelector('.toggle-paragraphs-button')
+
+slideToggleButton.addEventListener('click', function() {
+    box.slideToggle();
+})
+
+slideDownButton.addEventListener('click', function() {
+    content.slideDown(800, 'cubic-bezier(0.25, 0.1, 0.44, 1.4)')
+})
+
+slideUpButton.addEventListener('click', function() {
+    content.slideUp(1200, 'ease')
+})
+
+toggleParagraphsButton.addEventListener('click', function() {
+    paragraphs.forEach(p => {
+        p.slideToggle(600)
+    })
+})
---
+++ slide-down-up.js
@@ -0,0 +1,105 @@
+function slide(element, _speed, direction, easing) {
+    // prevent user from sliding down if already sliding
+    if(direction === 'down'
+        && (
+            element.classList.contains('setHeight')
+            || !element.classList.contains('hidden')
+        )
+    ) return false
+
+    // prevent user from sliding up if already sliding
+    if(direction === 'up'
+        && (
+            element.classList.contains('hidden')
+            || element.classList.contains('setHeight')
+        )
+    ) return false
+
+    const s = element.style
+    const speed = +_speed || 300
+    contentHeight = element.scrollHeight
+
+    // subtract padding from contentHeight
+    if(direction === 'up') {
+        const style = window.getComputedStyle(element)
+        const paddingTop = +style.getPropertyValue('padding-top').split('px')[0]
+        const paddingBottom = +style.getPropertyValue('padding-bottom').split('px')[0]
+        contentHeight = element.scrollHeight - paddingTop - paddingBottom
+    }
+
+    // create a setHeight CSS class
+    const sheet = document.createElement('style')
+    // create an id for each class to allow multiple elements to slide
+    // at the same time, such as when activated by a forEach loop
+    const setHeightId = (Date.now() * Math.random()).toFixed(0)
+    sheet.innerHTML = `.setHeight-${setHeightId} {height: ${contentHeight}px;}`
+    document.head.appendChild(sheet)
+
+    // add the CSS classes that will give the computer a fixed starting point
+    if(direction === 'up') {
+        element.classList.add(`setHeight-${setHeightId}`)
+    }
+    else {
+        element.classList.add('hidden', `setHeight-${setHeightId}`)
+    }
+
+    s.transition = `all ${speed}ms ${easing || ''}`
+    s.overflow = 'hidden'
+
+    // add/remove the CSS class(s) that will animate the element
+    if(direction === 'up') {
+        // Don't know why, but waiting 10 milliseconds before adding
+        // the 'hidden' class when sliding up prevents height-jumping
+        setTimeout(function() {
+            element.classList.add('hidden')
+        }, 10)
+    }
+    else {
+        element.classList.remove('hidden')
+    }
+
+    setTimeout(function() {
+        // remove the temporary inline styles and remove the temp stylesheet
+        element.removeAttribute('style')
+        sheet.parentNode.removeChild(sheet)
+        element.classList.remove(`setHeight-${setHeightId}`)
+    }, speed || 300)
+}
+
+function slideDownUpInit() {
+    const sheet = document.createElement('style')
+    sheet.id = 'slideCSSClasses'
+    sheet.innerHTML = `
+    .hidden {
+        height: 0 !important;
+        padding-top: 0 !important;
+        padding-bottom: 0 !important;
+        border-top-width: 0 !important;
+        border-bottom-width: 0 !important;
+        margin-top: 0 !important;
+        margin-bottom: 0 !important;
+        overflow: hidden;
+    }
+    `
+    document.head.appendChild(sheet)
+
+    Object.prototype.slideDown = function(_speed, easing) {
+        slide(this, _speed, 'down', easing)
+    }
+
+    Object.prototype.slideUp = function(_speed, easing) {
+        slide(this, _speed, 'up', easing)
+    }
+
+    Object.prototype.slideToggle = function(_speed, easing) {
+        if(this.classList.contains('hidden')) {
+            slide(this, _speed, 'down', easing)
+        }
+        else {
+            slide(this, _speed, 'up', easing)
+        }
+
+    }
+}
+
+// export default slideDownUpInit
